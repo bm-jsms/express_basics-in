80339e2be0faec2d44c2e3a6e6df7a6be6f2ab6d
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-20 21:08:30 -0500

    Express: Params

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ import express from 'express';
 const app = express();
 const port = 8000;
 
-// Regex
 app.get(/x/, (req, res) => {
 	res.send('<h1>This shows for any URL that contains the letter "x"</h1>');
+});
+
+app.get('/users/:userId/books/:bookId', (req, res) => {
+	const { userId, bookId } = req.params;
+	res.send(`<h1>User ID: ${userId}, Book ID: ${bookId}</h1>`);
 });
 
 app.listen(port, () => {
