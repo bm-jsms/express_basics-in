849ab576d7194c81db3724ab4d9d334c091f719c
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-20 20:58:12 -0500

    Express: Regex

--- index.js
+++ index.js
@@ -3,16 +3,9 @@ import express from 'express';
 const app = express();
 const port = 8000;
 
-app.get('/', (req, res) => {
-	res.send('<h1>Welcome to Express.js</h1>');
-});
-
-app.get('/about', (req, res) => {
-	res.send('<h1>About ⭐️</h1>');
-});
-
-app.use((req, res) => {
-	res.status(404).send('<h1>404 Page Not Found!</h1>');
+// Regex
+app.get(/x/, (req, res) => {
+	res.send('<h1>This shows for any URL that contains the letter "x"</h1>');
 });
 
 app.listen(port, () => {
