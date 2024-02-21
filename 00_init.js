a8500a8fbffe5e3422a8ef43bfc7caaa76e2efc7
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-20 20:12:36 -0500

    add: express basics

---
+++ 00_init.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+	res.send('<h1>Welcome to Express</h1>');
+});
+
+app.get('/about', (req, res) => {
+	res.send('<h1>About Us</h1>');
+});
+
+app.listen(port, () => {
+	console.log(`Server is running: http://localhost:${port}`);
+});
