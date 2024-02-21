bf3e8f72bca20696d5be7b6b1c883995b1aca86a
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-20 19:13:28 -0500

    add: without express

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
+	console.log(`Server is running: http://localhost:${port}`);
 });
