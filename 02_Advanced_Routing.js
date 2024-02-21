7f430185490d3e79037afb027054f3d66bbbd799
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-20 21:12:58 -0500

    add: Advanced Routing

---
+++ 02_Advanced_Routing.js
@@ -0,0 +1,28 @@
+import express from 'express';
+
+const app = express();
+const port = 8000;
+
+app.get(/x/, (req, res) => {
+	res.send('<h1>This shows for any URL that contains the letter "x"</h1>');
+});
+
+app.get('/users/:userId/books/:bookId', (req, res) => {
+	const { userId, bookId } = req.params;
+	res.send(`<h1>User ID: ${userId}, Book ID: ${bookId}</h1>`);
+});
+
+app.get(
+	'/double-callback',
+	(req, res, next) => {
+		console.log('The response will be sent by the next function...');
+		next();
+	},
+	(req, res) => {
+		res.send('<h1>Hello from D!</h1>');
+	},
+);
+
+app.listen(port, () => {
+	console.log(`Server is running: http://localhost:${port}`);
+});
